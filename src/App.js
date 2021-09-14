bb0640a83b36b5c5b00a96b22a451818adc72f50
lara <lara@vehicle4change.org>
2021-09-14 14:20:56 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
+// styles
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/magnific-popup.css";
+import "./assets/css/animate.css";
+import "./assets/css/main.css"; 
+import "./assets/css/responsive.css";
+import "./assets/css/renderPage.css";
 
-import Routes from "./Routes";
-import Home from "./components/pages/Home";
 
+// home page
+import Home from "./components/pages/Home.js";
+// portfolio sections
+import About from './components/pages/About.js';
+import Skills from './components/pages/Skills.js';
+import Experience from './components/pages/Experience.js';
+import Education from './components/pages/Education.js';
+import Portfolio from './components/pages/Portfolio.js';
+import Contact from './components/pages/Contact.js';
+//pages
+import Pdf1 from "./components/pages/Pdf1.js";
+import NoMatch from "./components/pages/Error404";
 
 function App() {
   return (
-    <Router>
-    <Home />
-      <Routes />
-    </Router>
+        <>
+
+<Route render={({location}) => (
+          <TransitionGroup>
+            <CSSTransition
+              key={location.key}
+              timeout={800}
+              classNames="fade"
+            >
+<Switch>
+<Switch>
+      <Route path="/"> <Home /></Route>
+       <Route path="/about"> <About /></Route>
+      <Route path="/skills"> <Skills /></Route>
+      <Route path="/experience"> <Experience /></Route>
+      <Route path="/education"> <Education /></Route>
+      <Route path="/portfolio"> <Portfolio /></Route>
+      <Route path="/contact"> <Contact /></Route>
+      <Route path="/resume"><Pdf1 /></Route>
+     <Route path="*"><NoMatch /> </Route>
+             </Switch>
+            </Switch>
+            </CSSTransition>
+          </TransitionGroup>
+        )} >
+        </Route>
+        </>
   );
 }
 
 export default App;
+
+        
